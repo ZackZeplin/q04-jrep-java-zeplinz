ac5607ed85fb8bd9a6803cad806342a7440d7b07
Zack Zeplin <zeplinz@live.unc.edu>
2019-11-17 07:28:18 +0000

    fixed shebang line and java usage bug

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 javac -d bin/ ./src/comp290/Jrep.java
 
--- src/comp290/Jrep.java
+++ src/comp290/Jrep.java
@@ -8,6 +8,10 @@ public class Jrep {
 
    public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
+      if (args.length == 0 || args[0].equals("--help")) {
+          System.out.println("Usage: jrep [PATTERN]");
+          System.exit(1);
+      }
       String match = args[0];
       while (sc.hasNextLine()) {
           String n = sc.nextLine();
